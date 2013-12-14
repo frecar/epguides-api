6fe9a3ac85bd7c52983e8572dde851313a18c63c
Rolf Erik Lekang <me@rolflekang.com>
2013-12-14 08:37:17 +0100

    use configparser and cfg files to fetch configs
    Sets debug to false and cache_type to simple by default.
    To change the default settings, add local.cfg with your settings.
    
    For available cache types see flask-cache documentation.

--- views.py
+++ views.py
@@ -1,15 +1,19 @@
+import ConfigParser
 from flask import Flask
 from flask.ext.cache import Cache
 
 from models import EpisodeNotFoundException, Show
 from utils import json_response
 
-app = Flask(__name__)
-app.config.update({
-    'DEBUG': True,
-    'CACHE_TYPE': 'redis'
-})
+config = ConfigParser.ConfigParser()
+config.read(['defaults.cfg', 'local.cfg'])
+CONFIG = {
+    'DEBUG': config.getboolean('flask', 'debug'),
+    'CACHE_TYPE': config.get('flask', 'cache_type')
+}
 
+app = Flask(__name__)
+app.config.update(CONFIG)
 cache = Cache(app)
 
 
