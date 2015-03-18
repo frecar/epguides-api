4d1f6330b505d583373c23dacdfbb85c7cb835fd
Fredrik Carlsen <fredrik@carlsen.io>
2015-03-18 21:17:42 +0100

    Adds setup.py

---
+++ setup.py
@@ -0,0 +1,42 @@
+# -*- coding: utf8 -*-
+from setuptools import setup, find_packages
+
+
+def _read_long_description():
+    try:
+        import pypandoc
+        return pypandoc.convert('README.md', 'rst', format='markdown')
+    except Exception:
+        return None
+
+setup(
+    name="epguides-api",
+    version='1.0.0',
+    url='http://github.com/frecar/epguides-api',
+    author='Fredrik Carlsen',
+    author_email='fredrik@carlsen.io',
+    description='API for epguides.com',
+    long_description=_read_long_description(),
+    packages=find_packages(exclude='tests'),
+    tests_require=[
+        'flask',
+        'flask-cache',
+        'redis',
+        'flake8',
+        'coverage',
+        'tox'
+    ],
+
+    license='MIT',
+    test_suite='runtests.runtests',
+    include_package_data=True,
+    classifiers=[
+        "Programming Language :: Python",
+        'Programming Language :: Python :: 2.7',
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        "Framework :: Flask",
+        "Environment :: Web Environment",
+        "Operating System :: OS Independent",
+        "Natural Language :: English",
+    ]
+)
