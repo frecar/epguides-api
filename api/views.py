debf11d9b303f07145039a6ed1fe4d32a825fd0e
Fredrik Carlsen <fredrik@carlsen.io>
2015-04-19 16:05:09 +0200

    Fix url paths api

--- api/views.py
+++ api/views.py
@@ -21,9 +21,10 @@ def discover_shows():
     for epguides_name in list_all_epguides_keys_redis():
 
         show = Show(epguides_name)
-        show.episodes = "{0}{1}/".format(app.config['BASE_URL'], epguides_name)
-        show.next_episode = "{0}{1}/next".format(app.config['BASE_URL'], epguides_name)
-        show.last_episode = "{0}{1}/last".format(app.config['BASE_URL'], epguides_name)
+        show.episodes = "{0}show/{1}/".format(app.config['BASE_URL'], epguides_name)
+        show.next_episode = "{0}show/{1}/next".format(app.config['BASE_URL'], epguides_name)
+        show.next_episode_released = "{0}show/{1}/next/released".format(app.config['BASE_URL'], epguides_name)
+        show.last_episode = "{0}show/{1}/last".format(app.config['BASE_URL'], epguides_name)
         show.imdb_url = "http://www.imdb.com/title/{0}".format(show.imdb_id)
         show.epguides_url = "http://www.epguides.com/{0}".format(epguides_name)
         result.append(show)
